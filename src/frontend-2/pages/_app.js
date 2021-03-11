701bb76b6b81acaeb1361ec0a04e7fb415ac4465
Jesse Hoogland <jessequinten@gmail.com>
2021-03-11 23:00:54 +0100

    ADD Started new frontend built with Next.js

---
+++ src/frontend-2/pages/_app.js
@@ -0,0 +1,52 @@
+import '../styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import Container from "@material-ui/core/Container"
+import Box from "@material-ui/core/Box"
+
+function MyApp({Component, pageProps}) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        // eslint-disable-next-line no-undef
+        const jssStyles = document.querySelector("#jss-server-side");
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    const title = `Health Curious | ${pageProps?.title}`;
+
+    return (
+        <>
+            <Head>
+                <title>Anki Squared</title>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="theme-color" content="#ffffff"/>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+            </Head>
+
+            <main>
+                <Box my={4}>
+                <Component {...pageProps} />
+                </Box>
+            </main>
+
+            <footer>
+                <Container>
+                <a
+                    href="https://github.com/jqhoogland"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {/* Author: Jesse Hoogland */}
+                </a>
+                </Container>
+            </footer>
+        </>)
+}
+
+export default MyApp
---
+++ src/frontend-2/pages/index.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect} from "react"
+import Head from 'next/head'
+import {FormControl, InputLabel, Select, MenuItem, Container} from "@material-ui/core"
+import {makeStyles} from "@material-ui/core"
+import useSWR from "swr"
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+
+
+const Chooser = ({label, chooseItem, defaultItem, path}) => {
+  const classes = useStyles()
+  const [item, setItem] = useState(defaultItem)
+  const [options, setOptions] = useState([defaultItem])
+
+  useEffect(() => {
+    // console.log(label, loadOptions)
+    // loadOptions().then(({result}) => {
+    //   console.log(label, result)
+    //   if (result && result.length) {
+    //     setItem(result[0])
+    //     setOptions(result)
+    //   }
+    // })
+  }, [])
+
+  const _setItem = (item) => {
+    setItem(item)
+    chooseItem(item)
+  }
+
+  return <FormControl className={classes.formControl}>
+    <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+    <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={item}
+        onChange={({target: {value}})=> _setItem(value)}
+    >
+      {options.map((option, i) => <MenuItem key={`key${i}`} value={option}>{option}</MenuItem>)}
+    </Select>
+  </FormControl>
+}
+
+
+export default function Home() {
+  const [deck, setDeck] = useState("Default")
+  const [model, setModel] = useState("Basic")
+
+  return (
+    <Container maxWidth="md">
+
+        <Chooser label="Deck" chooseItem={setDeck} defaultItem={deck} path="/api/decks"/>
+        <Chooser label="Model" chooseItem={setModel} defaultItem={model} path="/api/models"/>
+
+    </Container>
+  )
+}
