25d90f804867ed6c69e403bbb0837224eec30ddd
Jesse Hoogland <jessequinten@gmail.com>
2022-08-12 23:13:48 +0200

    ADD empty sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import { trpc } from "../utils/trpc";
 import { Field, Note } from '@prisma/client';
 import { NoteType } from '@prisma/client';
@@ -33,20 +33,35 @@ const Home: NextPage = () => {
         <meta name="description" content="An superpowered interface to your Anki notes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="mx-auto max-w-screen-md min-h-[100vh] py-16 px-8">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
-          Anki<sup className="text-sky-500">2</sup>
-        </h1>
-        <div className="flex flex-col gap-2">
-          {notes.map(note => (
-            <NoteRow note={note} type={getNoteType(note.ntid)} key={note.id.toString()} />
-          ))}
-        </div>
-      </main>
+      <Layout>
+        <main className="min-h-[100vh]py-16">
+          <div className="flex flex-col gap-2 divide-y">
+            {notes.map(note => (
+              <NoteRow note={note} type={getNoteType(note.ntid)} key={note.id.toString()} />
+            ))}
+          </div>
+        </main>
+      </Layout>
     </>
   );
 };
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="grid grid-cols-6">
+      <aside className="col-span-1 bg-base-300">
+        <header className="px-4 pt-4 pb-2 border-b">
+          <h1 className="text-3xl leading-normal font-extrabold ">
+            Anki<sup className="text-sky-500">2</sup>
+          </h1>
+        </header>
+      </aside>
+      <div className="col-span-5">
+        {children}
+      </div>
+    </div>
+  )
+}
 
 const NoteRow = ({ note, type }: { note: ParsedNote, type?: ParsedNoteType }) => {
   if (!type) {
